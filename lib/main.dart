46355ca1133e359f87cfda7ef5cb61589abc79b8
N4l4N <165954429+N4l4N@users.noreply.github.com>
2025-06-30 03:09:17 -0300

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,6 @@ class MainScreen extends StatelessWidget {
           ],
       ),
       body: ItemList(),
-      floatingActionButton: FloatingActionButton(onPressed:() => NewItemWindow(), child: Icon(Icons.add),),
       bottomNavigationBar: BottomAppBar(
           height: 60.0,
           color: const Color.fromARGB(255, 54, 116, 54),
@@ -51,7 +50,7 @@ class ItemList extends StatefulWidget {
 }
 
 class _ItemListState extends State<ItemList> {
-  //Item list to be filled and displayed
+  // Item list to be filled and displayed
   final List<Item> _itemList = [];
 
   // Object controllers
@@ -59,25 +58,131 @@ class _ItemListState extends State<ItemList> {
   final priceCtrl = TextEditingController();
   final qttCtrl = TextEditingController();
 
+  // List Managing functions
+  void _addItem(Item i){
+    setState(() {
+      _itemList.add(i);
+    });
+  }
+
+  void _removeItem(int i){
+    setState(() {
+      _itemList.removeAt(i);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _itemList.length,
-      itemBuilder:(context, i){
-        Card();
-      });
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: _itemList.length,
+            itemBuilder:(context, i){
+              final item = _itemList[i];
+              return Card(
+                child: ListTile(
+                  leading: Icon(Icons.shopping_bag),
+                  title: Text(item.name),
+                  subtitle: Text('${item.quantity} x ${item.price}: ${item.price * item.quantity}'),
+                  trailing: PopupMenuButton(
+                    itemBuilder: (context) => [
+                      PopupMenuItem(child: Row(children: [Icon(Icons.edit), Text('Edit')],)),
+                      PopupMenuItem(child: Row(children: [Icon(Icons.delete), Text('Delete')],)),
+                    ],
+                    icon: Icon(Icons.more_vert),
+                    ),
+                ),
+              );
+            }),
+          ),
+        NewItemWindow(onAdd: _addItem),
+      ],
+    );
   }
 }
 
 
 class NewItemWindow extends StatelessWidget {
-  const NewItemWindow({super.key});
+  final void Function(Item) onAdd;
+  NewItemWindow({required this.onAdd});
+
+  // Object controllers
+  final nameCtrl = TextEditingController();
+  final priceCtrl = TextEditingController();
+  final qttCtrl = TextEditingController();
+
+  void _funNewItem(BuildContext context){
+    showDialog(context: context, builder:(context){
+      return AlertDialog(
+        title: Text('New Item'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField( // ---------- Name field
+              controller: nameCtrl,
+              decoration: InputDecoration(
+                labelText: 'Item name',
+              ),
+            ),
+            TextField( // ---------- Price field
+              controller: priceCtrl,
+              decoration: InputDecoration(
+                labelText: 'Unit Price',
+              ),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+            ),
+            TextField( // ---------- Quantity field
+              controller: qttCtrl,
+              decoration: InputDecoration(
+                labelText: 'Quantity',
+              ),
+              keyboardType: TextInputType.number,
+            )
+          ],
+        ),
+        actions: [
+          TextButton(onPressed: Navigator.of(context).pop, child: Text('Cancel')), //Cancel new item
+          ElevatedButton(onPressed:(){ //Add new item
+            final newItem = Item(
+              name: nameCtrl.text,
+              price: double.tryParse(priceCtrl.text) ?? 0.0,
+              quantity: int.tryParse(qttCtrl.text) ?? 0,
+            );
+            onAdd(newItem);
+            Navigator.of(context).pop();
+          }, child: Text('Add Item')),
+        ],
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed:() => _funNewItem(context),
+              child: Icon(Icons.add),
+              ),
+            SizedBox(
+              width: 20.0,
+            )
+          ],
+        ),
+        SizedBox(
+          height: 10.0,
+        )
+      ],
+    );
   }
 }
+
+
 class Item{
   String name;
   double price;
